f3066c2fcc65bdeb507db270ecc0d39254d43744
BagusBintangF <bagusbintang77@gmail.com>
2024-10-21 15:23:13 +0700

    :sparkles: UI - update loading file page :zap:

--- src/components/fragments/loadingPage/index.jsx
+++ src/components/fragments/loadingPage/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import './style.css' 
 
 const LoadingPage = () => {
   return (
     <div className="fixed top-0 right-0 bottom-0 flex justify-center items-center w-full h-full bg-slate-400 opacity-40 z-30">
-      <p className="text-md">Loading</p>
+      <div className="loader" ></div>
     </div>
   )
 }
