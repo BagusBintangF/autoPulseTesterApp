12f8b1bb2bf9c5a39925b760c7c8cf1d150cba8d
BagusBintangF <bagusbintang77@gmail.com>
2024-10-22 18:14:17 +0700

    :sparkles: UI - update alert bar :zap:

--- src/components/fragments/alertBar/index.jsx
+++ src/components/fragments/alertBar/index.jsx
@@ -9,7 +9,7 @@ const AlertBar = (props) => {
         <div className="w-[24rem] h-[12rem] rounded bg-white text-gray-700 p-5 shadow-xl shadow-blue-gray-900/5 sticky top-32">
             <h1 className="text-xl font-bold pt-2 pb-2"> {props.title}</h1>
             <span className="flex flex-row">
-                <h2 className='text-md font-normal pt-2'>{`${props.message} :`}</h2>
+                <h2 className='text-md font-normal pt-2'>{`${props.message} : `}</h2>
                 <h2 className='text-md font-semibold pt-2'>{props.status}</h2>
             </span>
             <span className="absolute p-2 bottom-5 right-5">
